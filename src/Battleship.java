6b05cdc38f4fa3f3ce3ab2ef63508cbd438ed8f3
Tanner-Wills <tannerwills1@gmail.com>
2021-07-09 15:22:25 -0400

    created separate x and y ship coordinate lists

--- src/Battleship.java
+++ src/Battleship.java
@@ -6,27 +6,32 @@ public class Battleship {
         System.out.println("Welcome to Battleship!\n");
 
         // Player 1
-        int[] player1Ships = new int[5]; //Blank array for player ship coordinates
+        int[] player1ShipsX = new int[5]; //Blank array for player ship X coordinates
+        int[] player1ShipsY = new int[5]; //Blank array for player ship Y coordinates
         int ship = 1;
         int xCoord = 0;
         int yCoord = 0;
-        System.out.println("PLAYER 1, ENTER YOUR SHIP COORDINATES [X,Y]\n");
+        System.out.println("PLAYER 1, ENTER YOUR SHIP COORDINATES X Y\n");
 
-
+        //TODO: make the ship coordinate code into a separate method.
         Scanner input = new Scanner(System.in); // Create scanner object for user input
 
-
-        while (ship < 6){
+        //&& input.nextInt() < 5 && input.nextInt() >= 0
+        while(ship< 6){
             System.out.println("Enter ship " + ship + " location");
-            if (input.hasNextInt()){
+            //TODO: add constraints for xCoord and yCoord: min of 0, max of 5, is an int, hasnt already occured in the shipCoordinates
+
+
+            if (input.hasNextInt()) {
                 xCoord = input.nextInt();
 
-                if (input.hasNextInt()){
+                if (input.hasNextInt()) {
                     yCoord = input.nextInt();
                     input.nextLine();
                     System.out.println(xCoord + " " + yCoord);
-                    player1Ships[ship-1] = xCoord;
-                    ship ++;
+                    player1ShipsX[ship - 1] = xCoord;
+                    player1ShipsY[ship - 1] = xCoord;
+                    ship++;
 
                 } else {
                     System.out.println("Invalid Coordinates, Choose different coordinates");
@@ -37,10 +42,14 @@ public class Battleship {
                 input.nextLine();
             }
         }
-        System.out.println(player1Ships);
-
-
-    }
-
+        //Print Player 1 ship X coordinates after entry
+        System.out.println("PLAYER 1 SHIP X COORDINATES: \n");
+        for(int coord :player1ShipsX){
+            System.out.println(coord);
+        }
+        System.out.println("PLAYER 1 SHIP Y COORDINATES: \n");
+        for(int coord :player1ShipsY){
+            System.out.println(coord);
+        }
 
 }
