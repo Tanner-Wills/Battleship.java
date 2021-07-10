f98ebfc372d98fb1301a5432567ef14a028f11a8
Tanner-Wills <tannerwills1@gmail.com>
2021-07-10 17:14:19 -0400

    add game board grid

--- src/Battleship.java
+++ src/Battleship.java
@@ -7,70 +7,82 @@ public class Battleship {
     public static int player1Ships;
     public static int player2Ships;
     public static String[][] grid = new String[numRows][numCols];
-    public static int[][] missedGuess = new int[numRows][numRows];
+    public static int[][] hits = new int[numRows][numRows];
+    public static int[][] misses = new int[numRows][numRows];
 
     public static void main(String[] args) {
         // Initial Prompt
         System.out.println("Welcome to Battleship!\n");
 
-        // Step 1 -  Populate the player's ships
-        playerShipCoord();
-        
+        // Step 1 - Populate "grid" variable empty spaces
+        populateGrid();
+
+        // Step 2 - Identify player1Ships coordinates
+
+
+        // Step 3 -  Create the game board;
+        createGameBoard();
+
+
 }
 
-    public static void playerShipCoord(){
-        ArrayList<ArrayList<Integer>> player1Ships = new ArrayList<ArrayList<Integer>>(); //Blank array for player ship X coordinates
-        ArrayList<ArrayList<Integer>> player2Ships = new ArrayList<ArrayList<Integer>>(); //Blank array for player ship Y coordinates
-        int ship = 1;
-        int xCoord = 0;
-        int yCoord = 0;
-        ArrayList<Integer> shipCoord = new ArrayList<Integer>();
-        System.out.println("PLAYER 1, ENTER YOUR SHIP COORDINATES X Y\n");
-
-        Scanner input = new Scanner(System.in); // Create scanner object for user input
-
-        while (ship < 6) {
-            System.out.println("Enter ship " + ship + " location");
-
-            if (input.hasNextInt()) {
-                xCoord = input.nextInt();
-                System.out.println("xCoord = " + xCoord);
-
-                if (input.hasNextInt()) {
-                    yCoord = input.nextInt();
-                    System.out.println("yCoord = " + yCoord);
-                    shipCoord.clear();
-                    shipCoord.add(xCoord);
-                    shipCoord.add(yCoord);
-
-                    System.out.println("shipCoord = " + shipCoord);
-//&& !player1Ships.contains(shipCoord)
-                    if (xCoord >= 0 && xCoord < 5 && yCoord >= 0 && yCoord < 5 ){
-                        player1Ships.add(shipCoord);
-                        ship ++;
-                        input.nextLine();
-
-                    } else {
-                        System.out.println("Invalid Coordinates, Choose different coordinates");
-                        input.nextLine();
-                    }
-
-                } else {
-                    System.out.println("Invalid Coordinates, Choose different coordinates");
-                    input.nextLine();
+        public static void populateGrid(){
+            for(int rowEntry = 0; rowEntry < numRows; rowEntry ++){
+                for(int colEntry = 0; colEntry < numRows; colEntry ++){
+                    grid[rowEntry][colEntry] = " ";
                 }
+            }
+        }
 
-            } else {
-                System.out.println("Invalid Coordinates, Choose different coordinates");
-                input.nextLine();
+        public static void addPlayerShips(){
+
+
+
+        }
+
+        public static void createGameBoard(){
+            //create rows and columns of board
+            //Rows of "--" and columns of "|"
+            //between each row will be a row for entry of "X" for hit and "0" for miss
+            System.out.println();
+            System.out.println("    1     2     3     4     5");
+            System.out.println(" +-----+-----+-----+-----+-----+");
+            System.out.println("1|  " + grid[0][0] + "  |  " + grid[0][1]+ "  |  " + grid[0][2]+ "  |  " + grid[0][3]+ "  |  " + grid[0][4] + "  |");
+            System.out.println(" |-----+-----+-----+-----+-----|");
+            System.out.println("2|  " + grid[1][0] + "  |  " + grid[1][1]+ "  |  " + grid[1][2]+ "  |  " + grid[1][3]+ "  |  " + grid[1][4] + "  |");
+            System.out.println(" |-----+-----+-----+-----+-----|");
+            System.out.println("3|  " + grid[2][0] + "  |  " + grid[2][1]+ "  |  " + grid[2][2]+ "  |  " + grid[2][3]+ "  |  " + grid[2][4] + "  |");
+            System.out.println(" |-----+-----+-----+-----+-----|");
+            System.out.println("4|  " + grid[3][0] + "  |  " + grid[3][1]+ "  |  " + grid[3][2]+ "  |  " + grid[3][3]+ "  |  " + grid[3][4] + "  |");
+            System.out.println(" |-----+-----+-----+-----+-----|");
+            System.out.println("5|  " + grid[4][0] + "  |  " + grid[4][1]+ "  |  " + grid[4][2]+ "  |  " + grid[4][3]+ "  |  " + grid[4][4] + "  |");
+            System.out.println(" +-----+-----+-----+-----+-----+");
+
+
+
+
+
+
             }
 
-        }
-        //Print Player 1 ship X coordinates after entry
-        System.out.println("PLAYER 1 SHIP X COORDINATES: \n");
-        for (int i = 0; i < player1Ships.size(); i ++){
-            System.out.println(player1Ships.get(i));
-        }
-    }
 
-}
+
+            /*
+            System.out.println();
+            System.out.println("    1     2     3     4     5");
+            System.out.println(" +-----+-----+-----+-----+-----+");
+            System.out.println("1|     |     |     |     |     |");
+            System.out.println(" |-----+-----+-----+-----+-----|");
+            System.out.println("2|     |     |     |     |     |");
+            System.out.println(" |-----+-----+-----+-----+-----|");
+            System.out.println("3|     |     |     |     |     |");
+            System.out.println(" |-----+-----+-----+-----+-----|");
+            System.out.println("4|     |     |     |     |     |");
+            System.out.println(" |-----+-----+-----+-----+-----|");
+            System.out.println("5|     |     |     |     |     |");
+            System.out.println(" +-----+-----+-----+-----+-----+");
+            */
+        }
+
+
+
